68e1db938b994bf1a78f6380a017af499cd08214
Spencer Bard <sbard@phillies.com>
2019-06-25 00:53:36 -0400

    added print

--- pybigquery/sqlalchemy_bigquery.py
+++ pybigquery/sqlalchemy_bigquery.py
@@ -489,7 +489,7 @@ class BigQueryDialect(DefaultDialect):
             if val is None:
                 params[key] = "NULL"
             elif isinstance(val, datetime):
-                params[key] = datetime.strftime(val, "%Y-%m-%dT%H:%M:%S")
+                params[key] = datetime.strftime(val, "%Y-%m-%dT%H:%M:%S.000000")
             else:
                 params[key] = val
         print params
